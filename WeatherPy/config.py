343a3a944aea635450a9a705e8c5d97d09c7c951
gportes24 <gportes24@gmail.com>
2020-08-09 12:42:49 -0400

    completed project

---
+++ WeatherPy/config.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "6bf4166ff06c7518365da2c40025d418"
+
+# Google API Key
+g_key = "AIzaSyCFf5E5Jucsyujsn281Cv3m9vQ-HwwFKmQ"
+api_key = "AIzaSyCFf5E5Jucsyujsn281Cv3m9vQ-HwwFKmQ"
